71e18c4af282d007afd20d3080880a3f356f5294
Yuma Nakagome <officeyuma2024@gmail.com>
2025-10-19 15:35:56 +0900

    id

--- src/main/java/com/example/demo/entity/Todo.java
+++ src/main/java/com/example/demo/entity/Todo.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Table("todos")
 public class Todo {
+    @Id
     private Long id;
     private String title;
     private String description;
